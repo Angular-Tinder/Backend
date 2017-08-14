8b5f720993074c49e02a03a9fc2666296005b16e
VladimirAnaniev <vladeto97@gmail.com>
2017-08-14 17:26:47 +0300

    Added paging and sorting

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,23 @@ const User = require('../data/User')
 const express = require('express')
 
 const router = new express.Router()
+const PAGE_SIZE = 10
 
 router.get('/list', authCheck, (req, res) => {
   const user = req.user
   const interested = user.interested
+  const page = +req.query.page || 1
+  const search = req.query.search
 
   let query = {
     _id: { $ne: user._id }
   }
-  if (interested !== 'All') {
-    query.gender = user.interested
-  }
+  if (interested !== 'All') query.gender = user.interested
+  if (search) query.$text = { $search: search }
 
   User.find(query)
+  .skip((page - 1) * PAGE_SIZE)
+  .limit(PAGE_SIZE)
   .then(users => {
     res.json(users)
   })
