f0de2f94e8672cd2fa39a5a28f92926986df443a
VladimirAnaniev <vladeto97@gmail.com>
2017-08-14 13:58:08 +0300

    Implement liking and unliking of users

--- routes/match.js
+++ routes/match.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const Validator = require('../utilities/validator')
 const Match = require('../data/Match')
-const User = require('../data/User')
+// const User = require('../data/User')
 const authCheck = require('../auth/middleware/auth-check')
 
 const router = new express.Router()
@@ -9,44 +9,145 @@ const router = new express.Router()
 router.get('/mine', authCheck, (req, res) => {
   const user = req.user
 
-  Match.find({$or: [{'user1.user': user._id}, {'user2.user': user._id}]})
-  .sort('-timestamp')
-  .populate('user1.user')
-  .populate('user2.user')
-  .then(matches => {
-    res.status(200).json(matches)
+  Match.find({
+    $or: [{ 'user1.user': user._id }, { 'user2.user': user._id }],
+    $and: [{ 'user1.active': true }, { 'user2.active': true }]
   })
-  .catch(err => {
-    res.status(200).json(err)
-  })
-})
-
-router.post('/:id', authCheck, (req, res) => {
-  const user = req.user
-  const id = req.params.id
-  console.log(id)
-
-  User.findById(id)
-    .then(dbUser => {
-      console.log(dbUser)
-      if (!dbUser || user.email === dbUser.email) {
-        res.json({error: 'Error'}) // TODO: Proper error message
-      }
-
-      Match.create({
-        user1: {
-          user: user._id
-        },
-        user2: {
-          user: dbUser._id
-        }
-      })
-      .then(match => {
-        res.json(match)
-      })
+    .sort('-timestamp')
+    .populate('user1.user')
+    .populate('user2.user')
+    .then(matches => {
+      res.json(matches)
     })
     .catch(err => {
-      res.status(200).json(err)
+      return res.json({ success: false, message: err.message })
+    })
+})
+
+router.post('/unlike/:id', authCheck, (req, res) => {
+  const user = req.user
+  const id = req.params.id
+
+  // Find a potential match
+  Match.findOne({
+    $or: [
+      { $and: [{ 'user1.user': user._id }, { 'user2.user': id }] },
+      { $and: [{ 'user2.user': user._id }, { 'user1.user': id }] }
+    ]
+  })
+    .populate('user1.user')
+    .populate('user2.user')
+    .then(match => {
+      if (!match) {
+        // No such match - Error
+        return res.json({ success: false, message: 'You cannot unlike this user!' })
+      }
+
+      // Match exists - Set active to true or if already true return error message
+      if (match.user1.email === user.email) {
+        if (!match.user1.active) {
+          return res.json({
+            success: false,
+            message: 'You haven\'t liked this user!'
+          })
+        } else {
+          match.user1.active = false
+          match.save()
+          return res.json({
+            success: true,
+            message: 'Your unnlike was successful!'
+          })
+        }
+      } else {
+        if (!match.user2.active) {
+          return res.json({
+            success: false,
+            message: 'You haven\'t liked this user!'
+          })
+        } else {
+          match.user2.active = false
+          match.save()
+          return res.json({
+            success: true,
+            message: 'Your unlike was successful!'
+          })
+        }
+      }
+    })
+    .catch(err => {
+      return res.json({ success: false, message: err.message })
+    })
+})
+
+router.post('/like/:id', authCheck, (req, res) => {
+  const user = req.user
+  const id = req.params.id
+
+  // Find a potential match
+  Match.findOne({
+    $or: [
+      { $and: [{ 'user1.user': user._id }, { 'user2.user': id }] },
+      { $and: [{ 'user2.user': user._id }, { 'user1.user': id }] }
+    ]
+  })
+    .populate('user1.user')
+    .populate('user2.user')
+    .then(match => {
+      if (!match) {
+        // No such match - Create new Match
+        return Match.create({
+          user1: {
+            user: user._id,
+            active: true
+          },
+          user2: {
+            user: id
+          }
+        })
+          .then(match => {
+            return res.json({
+              success: true,
+              message: 'Your like was successful!'
+            })
+          })
+          .catch(err => {
+            return res.json({ success: false, message: err.message })
+          })
+      }
+
+      // Match exists - Set active to true or if already true return error message
+      if (match.user1.email === user.email) {
+        if (match.user1.active) {
+          return res.json({
+            success: false,
+            message: 'You have already liked this user.'
+          })
+        } else {
+          match.user1.active = true
+          match.save()
+          return res.json({
+            success: true,
+            message: 'Your like was successful!'
+          })
+        }
+      } else {
+        if (match.user2.active) {
+          return res.json({
+            success: false,
+            message: 'You have already liked this user.'
+          })
+        } else {
+          match.user2.active = true
+          match.save()
+          return res.json({
+            success: true,
+            message: 'Your like was successful!'
+          })
+        }
+      }
+    })
+    .catch(err => {
+      return res.json({ success: false, message: err.message })
     })
 })
 
