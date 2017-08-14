b949e4f9ef5e60dd8958475ab7fcb72b6a21826c
VladimirAnaniev <vladeto97@gmail.com>
2017-08-14 23:34:36 +0300

    Move profile to users routes and add get profile

--- routes/users.js
+++ routes/users.js
@@ -28,4 +28,49 @@ router.get('/list', authCheck, (req, res) => {
   })
 })
 
+router.post('/profile', authCheck, (req, res) => {
+  const user = req.user
+  const updated = req.body
+
+  if (updated.gender) user.gender = updated.gender
+  if (updated.interested) user.interested = updated.interested
+  if (updated.name) user.name = updated.name
+  if (updated.age) user.age = updated.age
+  if (updated.location) user.location = updated.location
+
+  user.save()
+    .then(updated => {
+      res.json({
+        success: true,
+        message: 'Profile updated successfully',
+        updated
+      })
+    })
+    .catch(err => {
+      res.json({
+        success: false,
+        message: err.message
+      })
+    })
+})
+
+router.get('/profile', authCheck, (req, res) => {
+  res.json(req.user)
+})
+
+router.get('/profile/:id', authCheck, (req, res) => {
+  const id = req.params.id
+
+  User.findById(id).then(user => {
+    if (!user) {
+      return req.json({success: false, message: 'No such user exists!'})
+    }
+
+    res.json(user)
+  })
+  .catch(err => {
+    res.json({success: false, message: err.message})
+  })
+})
+
 module.exports = router
